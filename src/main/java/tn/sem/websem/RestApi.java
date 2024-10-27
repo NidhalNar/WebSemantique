d9f89416857db6637fd01bf9a8ec23691bdb89ec
NidhalNar <124619795+NidhalNar@users.noreply.github.com>
2024-10-27 22:03:08 +0100

    SparkQl Queries

--- src/main/java/tn/sem/websem/RestApi.java
+++ src/main/java/tn/sem/websem/RestApi.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 public class RestApi {
 
+
     Model model = JenaEngine.readModel("data/rescuefood.owl");
 
     @GetMapping("/inventory")
